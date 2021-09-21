495d6576f42a06c8b0eb172b4ebc87161a6393a4
efe osah <onanefeosah@icloud.com>
2021-09-20 20:59:03 -0400

    made changes to scripts

--- Assets/Scripts/MoveIt.cs
+++ Assets/Scripts/MoveIt.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloWorld : MonoBehaviour
+public class MoveIt : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
