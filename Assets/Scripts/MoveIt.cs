5d722d8b79c87a141ee84a8d77c600a1e002810e
Jake <haas113@uwindsor.ca>
2021-09-22 13:54:11 -0400

    Moved camera, wrote MoveIt script, moved scene file

--- Assets/Scripts/MoveIt.cs
+++ Assets/Scripts/MoveIt.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class MoveIt : MonoBehaviour
 {
+    private Rigidbody rb; // kinematic rigidbodies are unaffected by physics so setting velocity doesn't work
+    private bool movingLeft = false;
+    private float vel = 1.0f / 50.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() // called 50 times per second
+    {
+        if (movingLeft)
+        {
+            gameObject.transform.position = new Vector3(
+                gameObject.transform.position.x - vel,
+                gameObject.transform.position.y,
+                gameObject.transform.position.z
+            );
+        }
+        else
+        {
+            gameObject.transform.position = new Vector3(
+                gameObject.transform.position.x + vel,
+                gameObject.transform.position.y,
+                gameObject.transform.position.z
+            );
+        }
+
+        if (movingLeft && gameObject.transform.position.x <= -3.0f)
+        {
+            movingLeft = false;
+        }
+        if (!movingLeft && gameObject.transform.position.x >= 3.0f)
+        {
+            movingLeft = true;
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
+
+
 }
