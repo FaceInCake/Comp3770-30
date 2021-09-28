626ae3c08cf360ea39b16fc7e4a341aa5543be1b
aisha-mali <62037398+aisha-mali@users.noreply.github.com>
2021-09-28 19:28:36 -0400

    Add files via upload

---
+++ Assets/ResizeIt.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class ResizeIt : MonoBehaviour
+{
+    public Vector3 local;
+    public float speed = 1f;//to store local scale
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        local= transform.localScale;
+        local.x += Time.deltatime;
+        transform.localScale = local;
+    }
+
+}
