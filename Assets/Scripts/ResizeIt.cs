cf56bd25aa2baffd96773c264f86d3038b8bcfbe
aisha-mali <62037398+aisha-mali@users.noreply.github.com>
2021-09-27 22:37:05 -0400

    Update ResizeIt.cs

--- Assets/Scripts/ResizeIt.cs
+++ Assets/Scripts/ResizeIt.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ResizeIt : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
+    public Vector3[] scale;
+
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine(ResizePerSecond());
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        IEnumerator ResizePerSecond();
+        while(true)
+        {
+            for(int i = 0; i < scale.Length; i++)
+            {
+                yield return new WaitForSeconds(1f);
+                transform.localScale = differentScale[i];
+                }
+        }
     }
 }
