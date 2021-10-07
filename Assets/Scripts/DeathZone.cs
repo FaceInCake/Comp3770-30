d44d932d1516202318e4296f9552d94c00814942
Jake <haas113@uwindsor.ca>
2021-10-07 18:56:22 -0400

    Added spawn locale

--- Assets/Scripts/DeathZone.cs
+++ Assets/Scripts/DeathZone.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 public class DeathZone : MonoBehaviour
 {
     GameObject player;
-    public Vector3 spawnPoint;
+    GameObject spawnLocale;
 
     void Start()
     {
         player = GameObject.Find("CapsuleCharacter").transform.GetChild(0).gameObject;
+        spawnLocale = GameObject.Find("SpawnLocale");
     }
 
     void OnTriggerEnter(Collider c)
     {
         if (c == player.GetComponent<Collider>())
         {
-            player.transform.position = spawnPoint;
+            player.transform.position = spawnLocale.transform.position;
             player.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
         }
     }
