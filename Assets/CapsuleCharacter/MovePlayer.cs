c3f4bde4ce379ee7eb88869e0f4a24a771ebe863
Matthew <faceincake@gmail.com>
2021-10-18 19:27:30 -0400

    Fixed Player Movement
    Atleast for me. Was using Update() without deltaTime and had some wonky counter-forces. Swapped in a non-versatile but robust division.

--- Assets/CapsuleCharacter/MovePlayer.cs
+++ Assets/CapsuleCharacter/MovePlayer.cs
@@ -5,10 +5,8 @@ using UnityEngine;
 // note: the player needs to have some friction because to stop jittering when the player stops, drag is disabled. So the player will never fully stop without friction.
 public class MovePlayer : MonoBehaviour
 {
-    public float maxHorizontalSpeed = 6.0f;
-    public float horizontalAcceleration = 10.0f;
-    public float jumpVelocity = 8.0f;
-    public float drag = 4.0f;
+    public float horizontalAcceleration;
+    public float jumpVelocity;
 
     GroundDetector groundDetector;
     Rigidbody rigidbody;
@@ -51,46 +49,18 @@ public class MovePlayer : MonoBehaviour
         // The local negative X axis is forwards
         // The local negative Z axis is left
         Vector3 moveVector = new Vector3(0, 0, 0);
-        if (Input.GetKey(KeyCode.D))
-        {
-            Vector3 forward = new Vector3(0.0f, 0.0f, 1.0f);
-            moveVector += forward;
-        }
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            Vector3 left = new Vector3(-1.0f, 0.0f, 0.0f);
-            moveVector += left;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            Vector3 backwards = new Vector3(0.0f, 0.0f, -1.0f);
-            moveVector += backwards;
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            Vector3 right = new Vector3(1.0f, 0.0f, 0.0f);
-            moveVector += right;
-        }
-
-        // applies control force to player
-        rigidbody.AddForce(transform.TransformDirection(moveVector.normalized) * horizontalAcceleration, ForceMode.Force);
-
+        if (Input.GetKey(KeyCode.W)) moveVector += Vector3.forward;
+        if (Input.GetKey(KeyCode.A)) moveVector += Vector3.left;
+        if (Input.GetKey(KeyCode.S)) moveVector += Vector3.back;
+        if (Input.GetKey(KeyCode.D)) moveVector += Vector3.right;
+        Vector3.Normalize(moveVector);
+        
         // applies drag to the player on x-z plane
         Vector3 xzVel = new Vector3(rigidbody.velocity.x, 0, rigidbody.velocity.z);
-
-        if (xzVel.magnitude > 0.1f)
-            rigidbody.AddForce(xzVel.normalized * -drag);
-
-        // controls the max speed of the player on the x-z plane
-        xzVel = new Vector3(rigidbody.velocity.x, 0, rigidbody.velocity.z);
-        if (xzVel.magnitude > maxHorizontalSpeed)
-        {
-            rigidbody.AddForce(xzVel.normalized * (maxHorizontalSpeed - xzVel.magnitude), ForceMode.VelocityChange);
-        }
-
+        if (xzVel.magnitude>=1) moveVector /= xzVel.magnitude;
+        
+        // Move
+        rigidbody.AddForce(moveVector*horizontalAcceleration*Time.deltaTime);
     }
 
 
