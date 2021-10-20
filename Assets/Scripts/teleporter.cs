5b307ee47aae1e940589bc07d57a6c04fad36eb7
Matthew <faceincake@gmail.com>
2021-10-20 16:00:34 -0400

    Added Coins and level cleanup
    Added another level, added coins to the levels and changed them a bit, teleporter sets the checkpoint.

--- Assets/Scripts/teleporter.cs
+++ Assets/Scripts/teleporter.cs
@@ -7,12 +7,13 @@ public class teleporter : MonoBehaviour
     public Vector3 tpPosition;
 
     private GameObject player;
-    private Collider collider;
+    private Transform spawn;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("CapsuleCharacter").transform.GetChild(0).gameObject;
+        spawn = GameObject.Find("SpawnLocale").transform;
     }
 
     void OnTriggerEnter(Collider c)
@@ -20,6 +21,7 @@ public class teleporter : MonoBehaviour
         if (c == player.GetComponent<Collider>())
         {
             player.transform.position = tpPosition;
+            spawn.position = tpPosition;
             player.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
         }
     }
