66e2e71ae1aea58aa983762644a916c9fdd4214b
Jake <haas113@uwindsor.ca>
2021-11-08 22:39:05 -0500

    added character controller to player

--- Assets/CapsuleCharacter/MovePlayer.cs
+++ Assets/CapsuleCharacter/MovePlayer.cs
@@ -2,72 +2,123 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// note: the player needs to have some friction because to stop jittering when the player stops, drag is disabled. So the player will never fully stop without friction.
 public class MovePlayer : MonoBehaviour
 {
-    public float horizontalAcceleration;
+    public float acceleration;
+    public float dragHorizontal;
+    public float gravity;
     public float jumpVelocity;
+    public float mouseSensitivityX;
 
-    GroundDetector groundDetector;
-    Rigidbody rigidbody;
-
-    int jumpCount = 0;
+    float speedModifier = 1.0f; // multiplies acceleration
+    
+    private bool isGrounded = true;
+    private Vector3 velocity = Vector3.zero;
+    
+    CharacterController controller;
 
     void Start()
     {
-        groundDetector = gameObject.transform.GetChild(0).GetComponent<GroundDetector>();
-        rigidbody = gameObject.GetComponent<Rigidbody>();
+        controller = gameObject.GetComponent<CharacterController>();
     }
 
 
-    bool wasColliding = false;
+    public Vector3 getForward()
+    {
+        return -gameObject.transform.right;
+    }
+
+    public Vector3 getBackward()
+    {
+        return gameObject.transform.right;
+    }
+
+    public Vector3 getLeft()
+    {
+        return -gameObject.transform.forward;
+    }
+
+    public Vector3 getRight()
+    {
+        return gameObject.transform.forward;
+    }
+
+
+    private void applyGravity()
+    {
+        velocity += Vector3.down * gravity * Time.deltaTime;
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = 0f;
+        }
+    }
+
+    private void jump()
+    {
+        velocity.y += jumpVelocity;
+    }
+
+    private void handleJumpInput()
+    {
+        if (isGrounded && Input.GetKey(KeyCode.Space))
+        {
+            jump();
+        }
+    }
+
+    private void handleCameraRotationInput()
+    {
+        float dx = Input.GetAxis("Mouse X");
+
+        gameObject.transform.Rotate(0, dx * mouseSensitivityX, 0, Space.World);
+
+    }
+
+    private void handleDirectionalInput()
+    {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            velocity += getForward() * (acceleration * speedModifier + dragHorizontal) * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            velocity += getLeft() * (acceleration * speedModifier + dragHorizontal) * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            velocity += getBackward() * (acceleration * speedModifier + dragHorizontal) * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            velocity += getRight() * (acceleration * speedModifier + dragHorizontal) * Time.deltaTime;
+        }
+    }
+
+    private void applyHorizontalDrag()
+    {
+        velocity -= new Vector3(velocity.x, 0, velocity.z) * dragHorizontal * Time.deltaTime;
+    }
+
+
+    // in the Unity docs the controller is manipulated in the Update function, not FixedUpdate
     void Update()
     {
 
-        // jump controls
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (groundDetector.isColliding)
-            {
-                jumpCount = 0;
-            }
+        isGrounded = controller.isGrounded;
 
-            if (jumpCount <= 1) {
-                jump();
-                jumpCount++;
-            }
-        }
+        applyGravity();
+        applyHorizontalDrag();
 
-        if (wasColliding == false && groundDetector.isColliding == true)
-        {
-            jumpCount = 1;
-        }
-        wasColliding = groundDetector.isColliding;
+        handleJumpInput();
+        handleDirectionalInput();
+        handleCameraRotationInput();
 
+        controller.Move(velocity * Time.deltaTime);
 
-        // WASD horizontal movement controls
-        // The local negative X axis is forwards
-        // The local negative Z axis is left
-        Vector3 moveVector = new Vector3(0, 0, 0);
-        if (Input.GetKey(KeyCode.W)) moveVector += Vector3.forward;
-        if (Input.GetKey(KeyCode.A)) moveVector += Vector3.left;
-        if (Input.GetKey(KeyCode.S)) moveVector += Vector3.back;
-        if (Input.GetKey(KeyCode.D)) moveVector += Vector3.right;
-        Vector3.Normalize(moveVector);
-        
-        // applies drag to the player on x-z plane
-        Vector3 xzVel = new Vector3(rigidbody.velocity.x, 0, rigidbody.velocity.z);
-        if (xzVel.magnitude>=1) moveVector /= xzVel.magnitude;
-        
-        // Move
-        rigidbody.AddForce(moveVector*horizontalAcceleration*Time.deltaTime);
     }
 
-
-    // when the player jumps, their y-velocity is 'set' to the jumpVelocity variable
-    // I tried just adding jumpVelocity to the current y-velocity but it isn't as satisfying as the current method
-    void jump()
-    {
-        rigidbody.AddForce(Vector3.up * (jumpVelocity - rigidbody.velocity.y), ForceMode.VelocityChange);
-    }
 }
